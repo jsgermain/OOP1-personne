4fa3782d2f3e7b5e04cd8f268a602dff1464b432
Johnes Saint Germain <johnbts@outlook.fr>
2019-09-13 12:52:32 +0200

    appelation de la méthode getAge

--- stageOOP/Program.cs
+++ stageOOP/Program.cs
@@ -13,10 +13,14 @@ namespace stageOOP
             personne unePersonne; // on a variable de la classe personne qui s'appelle unePersonne 
             unePersonne = new personne("Saint Germain", "Johnes", 31); // pour le constructeur puisse bien faire son travail, 
                                                                        //il faut que les paramètres soient respectés ("Saint Germain", "Johnes", 31)
+/*
+            // Pour changer l'age de la personne avec l'accesseur setAge créée dans la classe
+            unePersonne.setAge(26);
+*/
+            // on appelle la méthode getAge ciblant l'âge de la personne 
+            Console.WriteLine(unePersonne.getAge());
 
             
-            // on appelle la méthode d'affichage de la classe 
-            Console.WriteLine(unePersonne.affichage());
 
 
         }
