3ff4041b72622e7620ee3cf950ba9d227259fe40
Johnes Saint Germain <johnbts@outlook.fr>
2019-09-13 09:58:40 +0200

    Ajoutez des fichiers projet.

---
+++ stageOOP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stageOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            personne unePersonne; // on a variable de la classe personne qui s'appelle unePersonne
+            unePersonne = new personne(); // pour faire appel au constructeur de la classe personne, il faut instancier la classe. Cela permet d'arriver sur le constructeur qui s'éxecute 
+            
+            
+
+        }
+    }
+}
---
+++ stageOOP/personne.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stageOOP
+{
+    public class personne // la classe est en public pour pouvoir y accéder
+    {
+
+
+        // comme toute classe, celle là a une constructeur
+        public personne()
+        {
+            Console.WriteLine("coucou");
+        }
+    }
+}
