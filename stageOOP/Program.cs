e4e02f3c3988955db0f6d822c90f372c12e02308
Johnes Saint Germain <johnbts@outlook.fr>
2019-09-13 10:12:49 +0200

    enregistrer le fichier stageOOP

--- stageOOP/Program.cs
+++ stageOOP/Program.cs
@@ -10,7 +10,7 @@ namespace stageOOP
     {
         static void Main(string[] args)
         {
-            personne unePersonne; // on a variable de la classe personne qui s'appelle unePersonne
+            personne unePersonne; // on a variable de la classe personne qui s'appelle unePersonne 
             unePersonne = new personne(); // pour faire appel au constructeur de la classe personne, il faut instancier la classe. Cela permet d'arriver sur le constructeur qui s'éxecute 
             
             
